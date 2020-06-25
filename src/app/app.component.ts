5783c6922f395e5439816346e9972c54b7b6fc92
EnzoJ0se <enzojose.reus@gmail.com>
2020-06-25 19:12:14 -0300

    inicio do layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
+
+  @ViewChild('radarArea', { static: false }) radar: ElementRef;
+
   title = 'projeto-computacao-grafica';
+
+  constructor() { }
+
+  ngAfterViewInit() {
+    setTimeout(() => {
+      console.log(this.radar.nativeElement.style);
+
+    }, 800);
+  }
 }
