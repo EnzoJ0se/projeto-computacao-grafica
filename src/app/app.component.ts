34ef3f5c15ed14ba40c55576af13bba36acfb57d
EnzoJ0se <enzojose.reus@gmail.com>
2020-07-31 18:06:02 -0300

    funções de transformação e outros ajustes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,16 +7,7 @@ import { Component, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
 })
 export class AppComponent implements AfterViewInit {
 
-  @ViewChild('radarArea', { static: false }) radar: ElementRef;
-
-  title = 'projeto-computacao-grafica';
-
   constructor() { }
 
-  ngAfterViewInit() {
-    setTimeout(() => {
-      console.log(this.radar.nativeElement.style);
-
-    }, 800);
-  }
+  ngAfterViewInit() { }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    CommonModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
